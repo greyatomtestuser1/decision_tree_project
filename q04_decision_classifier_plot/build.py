06b27f71ebe49eadb7d0d8fe0e5e482e486d89c4
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-10-24 17:02:31 +0530

    Update build.py

--- q04_decision_classifier_plot/build.py
+++ q04_decision_classifier_plot/build.py
@@ -2,6 +2,7 @@
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -12,7 +13,7 @@ X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
-param_grid = {"max_depth": [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]}
+depth_list = [8, 10, 15, 20, 50, 100, 120, 150, 175, 200]
 
 
 # Write your solution here :
