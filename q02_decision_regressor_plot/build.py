a9f5cbe06e335267a1079ba21d491c34a10c103f
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-10-24 16:22:22 +0530

    Update build.py

--- q02_decision_regressor_plot/build.py
+++ q02_decision_regressor_plot/build.py
@@ -1,7 +1,7 @@
 # default imports
-from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_square_error
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,6 +11,7 @@ X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=9)
 
+depth_list = [2, 8, 10, 15, 20, 25, 30, 35, 45, 50]
 
 # Write your solution here :
 
